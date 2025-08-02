0d85daaff2f68e07ffb6fd4165fed6f4966cac2c
Deslre <2135232170@qq.com>
2025-08-02 14:09:29 +0800

    feat: 任务头部基本布局

--- src/renderer/src/components/task/TaskHeaderComponent.vue
+++ src/renderer/src/components/task/TaskHeaderComponent.vue
@@ -1,12 +1,59 @@
 <template>
     <div>
-        我是任务头部模板
+        <div class="taskheader-layout">
+            <div class="taskheader-layout-icon">
+                <AntIcon type="icon-91jiazai" />
+            </div>
+            <div class="taskheader-layout-input">
+                <el-input v-model="input" placeholder="Please input" />
+            </div>
+            <div class="taskheader-layout-button">
+                <el-button>
+                    <AntIcon type="icon-shanchu1" />
+                </el-button>
+                <el-switch v-model="preview" :active-action-icon="View" :inactive-action-icon="Hide"
+                    :active-value="true" :inactive-value="false" />
+                <span>预览模式</span>
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
+import AntIcon from '@renderer/utils/AntIcon';
+import { ref } from 'vue'
+import { Hide, View } from '@element-plus/icons-vue'
+
+const input = ref<string>('')
+const preview = ref<boolean>(true)
 
 </script>
 
 <style scoped lang="less">
+.taskheader-layout {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    height: 48px;
+    padding: 0 12px;
+    gap: 8px;
+
+    .taskheader-layout-icon {
+        display: flex;
+        align-items: center;
+    }
+
+    .taskheader-layout-input {
+        flex: 1;
+    }
+
+    .taskheader-layout-button {
+        margin-left: auto;
+        display: flex;
+        align-items: center;
+        gap: 8px;
+    }
+
+
+}
 </style>
